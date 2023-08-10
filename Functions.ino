bea817f1fa53e2ab14d2307ac2535d0de5f97a25
Lucas Martins Cardozo <95267389+lucaocardozo@users.noreply.github.com>
2023-08-10 20:47:21 -0300

    Putting comments on Functions.ino

--- Functions.ino
+++ Functions.ino
@@ -5,18 +5,21 @@
 #include <string>
 #include "Web.h"
 
-//Defining the pins
+//defines the pins that the servos are connected to.
 #define ServoPort1 D1
 #define ServoPort2 D2
 #define ServoPort3 D3
 #define ServoPort4 D4
 
 
-
+//create objects for the servos.
 Servo servoRotacao, servoGarra, servoCimaBaixo, servoFrenteTras;
+//create an object for the web server.
 ESP8266WebServer server(80);
 
+// This function handles requests to the root path.
 void handleRoot() {
+  // Send the HTML page to the client.
   server.send(200, "text/html", MAIN_page);
 }
 
@@ -37,39 +40,49 @@ void handleServo(){
   String POS4 = server.arg("POS4");
   int pos4 = std::atoi(POS4.c_str());
   servoFrenteTras.write(pos4);
-  
+
+  // Wait for 20 milliseconds.
   delay(20);
+  // Send a blank response to the client.
   server.send(200, "text/plane","");
 }
 
 //Setting up the WiFi network 
 void setup() {
+  // Set the SSID and password for the WiFi network.
   const char* ssid = "TCC2";
   const char* password = "lucascardozo";
+  // Initialize the serial port.
   Serial.begin(115200);
   delay(250);
 
+  // Attach the servos to the pins.
   servoRotacao.attach(ServoPort1);
   servoGarra.attach(ServoPort2);
   servoCimaBaixo.attach(ServoPort3);
   servoFrenteTras.attach(ServoPort4);
-  
-  WiFi.softAP(ssid, password);
 
+  // Start the WiFi access point.
+  WiFi.softAP(ssid, password);
+  // Get the IP address of the access point.
   IPAddress apip = WiFi.softAPIP();
+
+  // Print the SSID and IP address of the access point to the serial port.
   Serial.print("Connect your wifi laptop/mobile phone to this NodeMCU Access Point : ");
   Serial.println(ssid);
   Serial.print("Visit this IP : ");
   Serial.print(apip);
   Serial.println(" in your browser.");
-  
+
+  // Set up the web server.
   server.on("/",handleRoot);
   server.on("/setPOS",handleServo);
   server.begin();  
+  // Print a message to the serial port to indicate that the HTTP server is started.
   Serial.println("HTTP server started");
 }
 
-//Loop to run the program
+//This function loops forever and handles requests from the web server.
 void loop() {
  server.handleClient();
 }
